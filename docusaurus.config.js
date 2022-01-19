25b0f3029726c36a5f8d507dff856c3de2ed222d
Bharadwaj Giridhar <techydevil@gmail.com>
2022-01-19 08:14:48 +0530

    Changes footer copyright

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -116,7 +116,7 @@ const config = {
                             ],
                         },
                     ],
-                    copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+                    copyright: `Copyright © ${new Date().getFullYear()} Crewcharge.`,
                 },
                 prism: {
                     theme: lightCodeTheme,
